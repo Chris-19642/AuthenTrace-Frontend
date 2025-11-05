28533f897ba73d1525160f4fb5e198321c86ea97
Chris Reyes <u20231c399@upc.edu.pe>
2025-11-04 20:16:15 -0500

    Avance crear cuenta con acceso a bd

---
+++ src/app/services/crear-cuenta-service.ts
@@ -0,0 +1,19 @@
+import {inject, Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {environment} from '../environments/environment';
+import {Usuario} from '../model/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrearCuentaService {
+
+  private apiUrl = `${environment.apiURL}/usuario`;
+  private httpClient = inject(HttpClient);
+  constructor() {}
+
+  registrarCuenta(usuario: Usuario): Observable<Usuario> {
+    return this.httpClient.post<Usuario>(this.apiUrl, usuario);
+  }
+}
