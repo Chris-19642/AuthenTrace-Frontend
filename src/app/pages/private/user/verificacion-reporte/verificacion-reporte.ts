dc6e3cf955019939b87da2b22cf047f55f413ca4
Chris Reyes <u20231c399@upc.edu.pe>
2025-11-17 03:29:04 -0500

    Actualizando rama

---
+++ src/app/pages/private/user/verificacion-reporte/verificacion-reporte.ts
@@ -0,0 +1,34 @@
+import {Component, EventEmitter, Input, Output, } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {VerificacionResultado} from '../../../../model/verificacion-resultado';
+
+@Component({
+  selector: 'app-verificacion-reporte',
+  imports: [CommonModule],
+  templateUrl: './verificacion-reporte.html',
+  styleUrl: './verificacion-reporte.css',
+})
+export class VerificacionReporte {
+
+  @Input() resultado!: VerificacionResultado;
+  @Input() mostrarModal: boolean = false;
+  @Output() cerrar = new EventEmitter<void>();
+  @Output() descargar = new EventEmitter<void>();
+
+  // ► Propiedades calculadas para el HTML
+  get nombreDocumento(): string {
+    return this.resultado?.nombreArchivo ?? "Documento no identificado";
+  }
+
+  get resultadoValido(): boolean {
+    return this.resultado?.firmaValida ?? false;
+  }
+
+  get similitudPorcentaje(): number {
+    return this.resultado?.similitudPorcentaje ?? 0;
+  }
+
+  descargarPDF(): void {
+    this.descargar.emit();
+  }
+}
